60821e00bb6192fc4807a26ad2084863b07cee0a
Ahmed1khaled1 <ahmedkhaledg49@gmail.com>
2025-02-02 20:57:39 +0200

    Initial com

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,15 @@ import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import ChevronLeftOutlinedIcon from "@mui/icons-material/ChevronLeftOutlined";
 import { ShopContext } from "../context/ShopContext";
 
+export const Navbar = () => {
+  const { showSearch, setShowSearch, getCartCount, navigate } =
+    useContext(ShopContext);
+  const [visible, setVisible] = useState(false);
 
-export const Navbar = () => {    
-
-  const { showSearch, setShowSearch, getCartCount ,navigate } = useContext(ShopContext);
-  
-  const [visable, setVisable] = useState(false);
   return (
-    <div className="sticky top-0 bg-white z-50 flex justify-between items-center p-3 border-b border-gray-300">
+    <div className="sticky top-0 bg-white flex justify-between items-center p-3 border-b border-gray-300">
       <Link to="/">
-        <p className="text-xl font-bold ">CLOTHES SHOP</p>
+        <p className="text-xl font-bold">CLOTHES SHOP</p>
       </Link>
       <ul className="hidden md:flex gap-5">
         <NavLink to="/" className="flex flex-col items-center">
@@ -38,64 +37,64 @@ export const Navbar = () => {
       </ul>
       <div className="flex gap-5 items-center">
         <SearchOutlinedIcon
-          className=" cursor-pointer"
+          className="cursor-pointer"
           onClick={() => {
             setShowSearch(!showSearch);
             navigate("/collection");
           }}
         />
-        <div className=" relative">
+        <div className="relative">
           <PersonOutlineOutlinedIcon
             onClick={() => navigate("/login")}
-            className="w-5 cursor-pointer "
+            className="w-5 cursor-pointer"
           />
         </div>
         <Link to="/cart" className="relative flex items-center gap-1">
-          <ShoppingCartOutlinedIcon className="w-5 cursor-pointer " />
-          <p className="absolute bg-red-500 text-white aspect-square rounded-full text-[12px] left-3 top-3 w-5 text-center leading-5 ">
+          <ShoppingCartOutlinedIcon className="w-5 cursor-pointer" />
+          <p className="absolute bg-red-500 text-white aspect-square rounded-full text-[12px] left-3 top-3 w-5 text-center leading-5">
             {getCartCount()}
           </p>
         </Link>
         <div className="w-5 cursor-pointer block md:hidden">
-          <MenuOutlinedIcon onClick={() => setVisable(true)} />
+          <MenuOutlinedIcon onClick={() => setVisible(true)} />
         </div>
         <div
-          className={`flex flex-col absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all  ${
-            visable ? `w-full` : `w-0`
+          className={`z-50 flex flex-col absolute top-0 right-0 h-screen overflow-hidden bg-white transition-all ${
+            visible ? `w-full` : `w-0`
           }`}
         >
           <div
-            onClick={() => setVisable(false)}
+            onClick={() => setVisible(false)}
             className="flex items-center gap-2 cursor-pointer p-4 border-b-2 border-gray-400 hover:text-blue-600"
           >
-            <ChevronLeftOutlinedIcon className="h-5 " />
-            <p className="text-xl  font-semibold hover:text-blue-600">back</p>
+            <ChevronLeftOutlinedIcon className="h-5" />
+            <p className="text-xl font-semibold hover:text-blue-600">back</p>
           </div>
           <NavLink
-            onClick={() => setVisable(false)}
+            onClick={() => setVisible(false)}
             to="/"
             className="cursor-pointer py-3 px-10 border-b-2 border-gray-400 text-xl font-semibold hover:text-blue-600"
           >
             HOME
           </NavLink>
           <NavLink
-            onClick={() => setVisable(false)}
+            onClick={() => setVisible(false)}
             to="/collection"
             className="cursor-pointer py-3 px-10 border-b-2 border-gray-400 text-xl font-semibold hover:text-blue-600"
           >
             COLLECTION
           </NavLink>
           <NavLink
-            onClick={() => setVisable(false)}
+            onClick={() => setVisible(false)}
             to="/about"
-            className="cursor -poicursor-pointer py-3 px-10 border-b-2 border-gray-400 text-xl font-semibold hover:text-blue-600"
+            className="cursor-pointer py-3 px-10 border-b-2 border-gray-400 text-xl font-semibold hover:text-blue-600"
           >
             ABOUT
           </NavLink>
           <NavLink
-            onClick={() => setVisable(false)}
+            onClick={() => setVisible(false)}
             to="/contact"
-            className="cursor -poicursor-pointer py-3 px-10 border-b-2 border-gray-400 text-xl font-semibold hover:text-blue-600"
+            className="cursor-pointer py-3 px-10 border-b-2 border-gray-400 text-xl font-semibold hover:text-blue-600"
           >
             CONTACT
           </NavLink>
